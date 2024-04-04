f1ecd365e980eadde25a10adc3086cc337ced2e8
Lucky-Tsuma <tsumahmumba@gmail.com>
2024-04-04 06:01:35 +0300

    feat: add biostar settings doctype

---
+++ biostar_sync/biostar_sync/doctype/biostar_sync_settings/biostar_sync_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Lucky Tsuma and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Biostar Sync Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ biostar_sync/biostar_sync/doctype/biostar_sync_settings/test_biostar_sync_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Lucky Tsuma and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBiostarSyncSettings(FrappeTestCase):
+	pass
